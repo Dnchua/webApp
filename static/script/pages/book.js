ae0db5a71f8f36f5cc43b4cb5250e0aae9e98706
Dch <1165073373@qq.com>
2018-07-02 20:53:25 +0800

    接口爆炸乐

--- static/script/pages/book.js
+++ static/script/pages/book.js
@@ -16,73 +16,73 @@ $.get('/ajax/book',{
     id : chapter_id
     },
     function (d) {
+    if (d.adverRead == true) {
         let hasThisBook = JSON.parse(Util.StorageGetter('bookIdList'));
         let flag,//flag为true时表示书架里没有这本书，可以添加
             color;
-        if (hasThisBook.indexOf(chapter_id)!=-1){
+        if (hasThisBook.indexOf(chapter_id) != -1) {
             flag = false;
             color = 'colorChange';
-        }else{
+        } else {
             flag = true;
             color = '';
         }
         var windowWidth = $(window).width();
-        if(windowWidth<320){
-            windowWidth=320;
+        if (windowWidth < 320) {
+            windowWidth = 320;
         }
         var data_book = d;
         new Vue({
-            el:'#app',
-            data :{
-                screen_width:windowWidth,
-                items : data_book,
-                show : true,
-                flag_shelf:flag,
-                controlColor:color
+            el: '#app',
+            data: {
+                screen_width: windowWidth,
+                items: data_book,
+                show: true,
+                flag_shelf: flag,
+                controlColor: color
             },
-            methods:{
-                storeShelf : function () {
+            methods: {
+                storeShelf: function () {
                     var bookIdList = 'bookIdList',
                         bookList = 'bookList';
-                    if (flag && !(Util.StorageGetter(bookIdList)===null)){
+                    if (flag && !(Util.StorageGetter(bookIdList) === null)) {
                         var arr1 = JSON.parse(Util.StorageGetter(bookIdList));
                         var arr2 = JSON.parse(Util.StorageGetter(bookList));
                         arr1.push(chapter_id);
                         arr2.push(d);
-                        Util.StorageSetter(bookIdList,JSON.stringify(arr1));
-                        Util.StorageSetter(bookList,JSON.stringify(arr2));
+                        Util.StorageSetter(bookIdList, JSON.stringify(arr1));
+                        Util.StorageSetter(bookList, JSON.stringify(arr2));
                         this.flag_shelf = false;
                         flag = false;
                         this.controlColor = 'colorChange';
-                    }else if (flag ){
-                        var arr1=[],
-                            arr2=[];
+                    } else if (flag) {
+                        var arr1 = [],
+                            arr2 = [];
                         arr1.push(chapter_id);
                         arr2.push(d);
-                        Util.StorageSetter(bookIdList,JSON.stringify(arr1));
-                        Util.StorageSetter(bookList,JSON.stringify(arr2));
+                        Util.StorageSetter(bookIdList, JSON.stringify(arr1));
+                        Util.StorageSetter(bookList, JSON.stringify(arr2));
                         this.flag_shelf = false;
                         flag = false;
                         this.controlColor = 'colorChange';
                     }
-                    else if (!flag){
+                    else if (!flag) {
                         let arr1 = JSON.parse(Util.StorageGetter(bookIdList));
                         let arr2 = JSON.parse(Util.StorageGetter(bookList));
                         let count = arr1.indexOf(chapter_id);
                         arr1.splice(count, 1);
                         arr2.splice(count, 1);
-                        Util.StorageSetter(bookIdList,JSON.stringify(arr1));
-                        Util.StorageSetter(bookList,JSON.stringify(arr2));
+                        Util.StorageSetter(bookIdList, JSON.stringify(arr1));
+                        Util.StorageSetter(bookList, JSON.stringify(arr2));
                         flag = true;
                         this.flag_shelf = true;
                         this.controlColor = '';
                     }
                 }
             },
-            mouted : {
-
-            }
+            mouted: {}
         })
-},'json')
+    }
+},'json');
 
 
